f1c9142b7573edcaaf9eaa9386fff9d61aa114b2
kuldeepdiggibyte <kp.managuli2000@gmail.com>
2024-03-18 17:17:52 +0530

    assignment_2 create product_details table and performed operations

---
+++ assignment_2.sql
@@ -0,0 +1,23 @@
+use ecommerce
+go
+
+CREATE TABLE product_details (
+    sell_date DATE,
+    product VARCHAR(100)
+);
+INSERT INTO product_details (sell_date, product) VALUES 
+('2020-05-30', 'Headphones'),
+('2020-06-01', 'Pencil'),
+('2020-06-02', 'Mask'),
+('2020-05-30', 'Basketball'),
+('2020-06-01', 'Book'),
+('2020-06-02', 'Mask'),
+('2020-05-30', 'T-Shirt');
+
+SELECT 
+    sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    STRING_AGG(product, ', ') AS product_list
+FROM product_details
+GROUP BY sell_date;
+
