f02339b4e46e881178ea2eb47238fe6d48c65797
kuldeepdiggibyte <kp.managuli2000@gmail.com>
2024-03-18 17:38:58 +0530

    assignment_3 contain department name with employee name and show total salary

---
+++ assignment_3.sql
@@ -0,0 +1,27 @@
+'''Table creation'''
+
+CREATE TABLE dept_tbl (
+    id_deptname VARCHAR(50),
+    emp_name VARCHAR(50),
+    salary INT
+);
+
+'''Inserting values into table'''
+
+INSERT INTO dept_tbl (id_deptname, emp_name, salary) VALUES 
+('1111-MATH', 'RAHUL', 10000),
+('1111-MATH', 'RAKESH', 20000),
+('2222-SCIENCE', 'AKASH', 10000),
+('222-SCIENCE', 'ANDREW', 10000),
+('22-CHEM', 'ANKIT', 25000),
+('3333-CHEM', 'SONIKA', 12000),
+('4444-BIO', 'HITESH', 2300),
+('44-BIO', 'AKSHAY', 10000);
+
+'''query to show name of department and total salary'''
+SELECT 
+    id_deptname,
+    SUM(salary) AS total_salary
+FROM dept_tbl
+GROUP BY id_deptname;
+
